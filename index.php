ac8e841bf3bdaf709c635d108a9a0f4b17f78c7b
paitorre123 <paitorre@alumnos.ubiobio.cl>
2020-08-05 10:39:50 -0400

    inicio del curso php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        /* incluye archivo externo */
+        include("utilidades_index.php");
+        /* el punto concatema
+           la coma separa*/      
+        $mensaje = 'Hola mundo!!';
+        echo '<p> primero: ' . $mensaje . '</p>';// commilla simple reconocimiento literal
+        echo "<p> segundo: $mensaje </p>";//comillas dobles reconocimiento de variables
+        dame_datos("tercero");
+        dame_datos("cuarto");
+    ?>
+
+</body>
+</html>
---
+++ utilidades_index.php
@@ -0,0 +1,5 @@
+<?php
+    function dame_datos($lugar){
+        echo "<p> $lugar: funcion dame datos!!</p>";
+    }
+?>
